b3211ada4dc60006b3871e6dfdccb8acb37a48fc
[mihir1782] <[mjoshi1782@gmail.com]>
2025-02-08 16:36:59 -0500

    1:39:00 - CRUD

--- base/views.py
+++ base/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
-
+from .models import Room
 
 # Create your views here.
 # creating dictionaries : rooms with key (id) & value (name) pairs
-rooms = [
-    {'id': 1, 'name':'lets learn python!'},
-    {'id': 2, 'name':'Design with me'},
-    {'id': 3, 'name':'frontend developers'},
-]
+
+
+# rooms = [
+#     {'id': 1, 'name':'lets learn python!'},
+#     {'id': 2, 'name':'Design with me'},
+#     {'id': 3, 'name':'frontend developers'},
+# ]
+
 def home(request):
+    rooms = Room.objects.all()
     context ={ 'rooms': rooms}
     return render(request, 'base/home.html',context)
 
 def room(request,pk):    
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context ={'room': room}
     return render(request,'base/room.html',context)
