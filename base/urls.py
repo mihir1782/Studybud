0b6b51d153fd72ae09dabdbfa8468e37a1e3d9f2
[mihir1782] <[mjoshi1782@gmail.com]>
2025-02-08 12:27:10 -0500

    31:28- Views & URL's

---
+++ base/urls.py
@@ -0,0 +1,8 @@
+# it is just specific for some app\
+from django.urls import path
+from .import views
+
+urlpatterns =[
+    path('',views.home,name="home"),
+    path('room/',views.room,name="ROOM"),
+]
---
+++ studybud/urls.py
@@ -0,0 +1,12 @@
+# it is full for full project (root directory)
+from django.contrib import admin
+from django.urls import path,include
+
+
+    
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('base.urls'))
+]
+
